f059df2f35948e42711e45abcff3f18653ed91fb
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-02 11:25:13 -0300

    a common function1!!!
    * Added #cantModify

--- src/main/java/me/lukiiy/flobby/Echo.java
+++ src/main/java/me/lukiiy/flobby/Echo.java
@@ -1,6 +1,8 @@
 package me.lukiiy.flobby;
 
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -11,6 +13,16 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class Echo implements Listener {
     private static final String MODIFY_PERMISSION = "flobby.modify";
 
+    /**
+     * The default check for most of the event handlers here!
+     * @param world event world
+     * @param player the player who caused it
+     * @return A true if they SHOUDN'T modify, false otherwise.
+     */
+    private boolean cantModify(World world, Player player) {
+        return (world == Flobby.getInstance().getWorld() && !player.hasPermission(MODIFY_PERMISSION));
+    }
+
     @EventHandler
     public void join(PlayerJoinEvent e) {
         Location loc = Flobby.getInstance().getMain();
@@ -20,16 +32,21 @@ public class Echo implements Listener {
 
     @EventHandler
     public void blockPlace(BlockPlaceEvent e) {
-        if (e.getBlock().getWorld() == Flobby.getInstance().getWorld() && !e.getPlayer().hasPermission(MODIFY_PERMISSION)) e.setCancelled(true);
+        if (cantModify(e.getBlock().getWorld(), e.getPlayer())) {
+            e.setBuild(false);
+            e.setCancelled(true);
+        }
     }
 
     @EventHandler
     public void blockBreak(BlockBreakEvent e) {
-        if (e.getBlock().getWorld() == Flobby.getInstance().getWorld() && !e.getPlayer().hasPermission(MODIFY_PERMISSION)) e.setCancelled(true);
+        if (cantModify(e.getBlock().getWorld(), e.getPlayer())) e.setCancelled(true);
     }
 
     @EventHandler
     public void drop(PlayerDropItemEvent e) { // TODO: only deny lobby items, when I implement those...
-        if (e.getPlayer().getWorld() == Flobby.getInstance().getWorld() && !e.getPlayer().hasPermission(MODIFY_PERMISSION)) e.setCancelled(true);
+        Player p = e.getPlayer();
+
+        if (cantModify(p.getWorld(), p)) e.setCancelled(true);
     }
 }
