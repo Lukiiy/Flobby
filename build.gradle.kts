d449a13c74f0947347a642e983775c27694bf795
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-02 10:30:48 -0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly(files("lib/paper2.jar"))
+}
+
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+tasks {
+    processResources {
+        val props = mapOf("version" to version, "description" to project.description)
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
