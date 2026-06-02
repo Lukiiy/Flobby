9826302cb40076833683bf30052def3b115f325d
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-02 10:34:13 -0300

    Yeah, that's it for now, really...
    * Added Main class, which implements BaseLobby
    * Added code to #sendToLobby
    * Added Listener class
    * Added main 3 event listeners
    * Added Location serialization/deserialization

---
+++ src/main/java/me/lukiiy/flobby/Echo.java
@@ -0,0 +1,29 @@
+package me.lukiiy.flobby;
+
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Echo implements Listener {
+    private static final String MODIFY_PERMISSION = "flobby.modify";
+
+    @EventHandler
+    public void join(PlayerJoinEvent e) {
+        Location loc = Flobby.getInstance().getMain();
+
+        if (loc != null) e.getPlayer().teleport(loc);
+    }
+
+    @EventHandler
+    public void blockBreak(BlockBreakEvent e) {
+        if (e.getBlock().getWorld() == Flobby.getInstance().getWorld() && !e.getPlayer().hasPermission(MODIFY_PERMISSION)) e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void blockBreak(BlockPlaceEvent e) {
+        if (e.getBlock().getWorld() == Flobby.getInstance().getWorld() && !e.getPlayer().hasPermission(MODIFY_PERMISSION)) e.setCancelled(true);
+    }
+}
---
+++ src/main/java/me/lukiiy/flobby/Flobby.java
@@ -0,0 +1,126 @@
+package me.lukiiy.flobby;
+
+import me.lukiiy.flow.BaseLobby;
+import me.lukiiy.flow.Flow;
+import me.lukiiy.flow.FlowPlayer;
+import me.lukiiy.flow.component.BasePlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jspecify.annotations.NonNull;
+
+public final class Flobby extends JavaPlugin implements BaseLobby {
+    private Location main = null;
+    private double boostY = -1;
+    private double cutOffRadius = -1;
+
+    @Override
+    public void onEnable() {
+        setupConfig();
+        reloadVars();
+
+        getServer().getPluginManager().registerEvents(new Echo(), this);
+        Flow.getInstance().getManager().setLobby(this);
+    }
+
+    public static Flobby getInstance() {
+        return JavaPlugin.getPlugin(Flobby.class);
+    }
+
+    public void setMain(Location main) {
+        this.main = main;
+        getConfig().set("pos", serialize(main));
+
+        saveConfig();
+    }
+
+    public Location getMain() {
+        return main;
+    }
+
+    public void setBoostY(double boostY) {
+        this.boostY = boostY;
+        getConfig().set("boostY", boostY);
+
+        saveConfig();
+    }
+
+    public double getBoostY() {
+        return boostY;
+    }
+
+    public void setCutOffRadius(double cutOffRadius) {
+        this.cutOffRadius = cutOffRadius;
+        getConfig().set("cutOffRadius", cutOffRadius);
+
+        saveConfig();
+    }
+
+    public double getCutOffRadius() {
+        return cutOffRadius;
+    }
+
+    public World getWorld() {
+        return main.getWorld();
+    }
+
+    // Config
+    public void setupConfig() {
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public void reloadVars() {
+        main = deserialize(getConfig().getString("pos", null));
+        boostY = getConfig().getDouble("boostY", -1);
+        cutOffRadius = getConfig().getDouble("cutOffRadius", -1);
+    }
+
+    @Override
+    public void sendToLobby(@NonNull BasePlayer basePlayer) {
+        if (!(basePlayer instanceof FlowPlayer fp)) return;
+
+        Player player = fp.getPlayer();
+
+        player.setHealth(20);
+        player.setSaturation(20);
+        player.setFoodLevel(20);
+        player.setFlying(false);
+        player.setFireTicks(0);
+
+        if (player.isDead()) player.spigot().respawn();
+
+        player.setGameMode(GameMode.ADVENTURE);
+        player.setExperienceLevelAndProgress(0);
+        player.clearTitle();
+        player.clearActiveItem();
+        player.clearActivePotionEffects();
+        player.getInventory().clear();
+    }
+
+    // Utils
+    public static String serialize(Location loc) { // world;x;y;z;yaw;pitch
+        if (loc == null) return null;
+
+        World world = loc.getWorld();
+        String worldName = world == null ? "" : world.getName();
+
+        return String.format("%s;%f;%f;%f;%f;%f", worldName, loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+    }
+
+    public static Location deserialize(String data) {
+        if (data == null || data.isBlank()) return null;
+
+        String[] p = data.split(";", -1);
+        if (p.length != 6) return null; // incomplete/missing data
+
+        World world = p[0].isEmpty() ? null : Bukkit.getWorld(p[0]);
+        if (!p[0].isEmpty() && world == null) return null; // unknown world
+
+        return new Location(world, Double.parseDouble(p[1]), Double.parseDouble(p[2]), Double.parseDouble(p[3]), Float.parseFloat(p[4]), Float.parseFloat(p[5]));
+    }
+}
