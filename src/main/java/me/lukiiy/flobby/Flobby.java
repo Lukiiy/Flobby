dd7c98e19c86bd73bc5117a285526b7a51079f75
Lukiiy <lukiiy@proton.me>
2026-07-27 11:52:16 -0300

    remove location utils from Flobby

--- src/main/java/me/lukiiy/flobby/Flobby.java
+++ src/main/java/me/lukiiy/flobby/Flobby.java
@@ -113,29 +113,4 @@ public final class Flobby extends JavaPlugin implements BaseLobby {
         getWorld().setGameRule(GameRule.DO_INSOMNIA, false);
         getWorld().setGameRule(GameRule.SHOW_DEATH_MESSAGES, false);
     }
-
-    // Utils
-    public static String serialize(Location loc) { // world;x;y;z;yaw;pitch
-        if (loc == null) return null;
-
-        World world = loc.getWorld();
-
-        return String.format(Locale.US, "%s;%f;%f;%f;%f;%f", world == null ? "" : world.getName(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
-    }
-
-    public static Location deserialize(String data) {
-        if (data == null || data.isBlank()) return null;
-
-        String[] parts = data.split(";", -1);
-        if (parts.length != 6) return null; // incomplete/missing data
-
-        World world = parts[0].isEmpty() ? null : Bukkit.getWorld(parts[0]);
-        if (!parts[0].isEmpty() && world == null) return null; // unknown world
-
-        try {
-            return new Location(world, Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]), Float.parseFloat(parts[4]), Float.parseFloat(parts[5]));
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
 }
