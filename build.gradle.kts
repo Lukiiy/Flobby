ebee8a6749de48dd23745ed7afe35d1d8d879df4
Lukiiy <lukiiy@proton.me>
2026-07-29 18:56:29 -0300

    Add ViaVersion as optional dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,13 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.viaversion.com")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    compileOnly(files("lib/paper2.jar"))
+    compileOnly(files("lib/paper0.jar"))
+    compileOnly("com.viaversion:viaversion-api:5.11.0")
 }
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(25)
